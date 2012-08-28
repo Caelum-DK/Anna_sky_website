729d6bef0177d5b31dd38d2cef7d9327bfb5bfb6
seedorff <seedorff@caelum.dk>
2012-08-28 17:25:15 +0200

    mongol

--- frontpage.php
+++ frontpage.php
@@ -31,10 +31,10 @@ get_header(); ?>
 			</h2>
 			
 			<!-- Display the Page's Content in a div box. -->
-			<div class="entry five columns centered">
+			<div class="entry five columns first-child">
 				<?php the_content(); ?>
 			</div>
-			<div class="entry five columns centered">
+			<div class="entry five columns first-child">
 				<?php the_field( "2nd_row" ); ?>
 			</div>
 		</div>
